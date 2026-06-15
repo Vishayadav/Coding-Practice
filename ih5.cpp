d4fd094a0a310444da4f2738627a357116c9d4d7
Visha Yadav <visha2.yadav1@gmail.com>
2026-06-15 10:03:52 +0530

    Sentence input

---
+++ ih5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin, s);
+    cout << s;
+    return 0;
+}
