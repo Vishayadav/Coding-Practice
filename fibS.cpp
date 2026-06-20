7bda9a07bdfa40a6250519c52ee7c07c69c43fee
Visha Yadav <visha2.yadav1@gmail.com>
2026-06-20 12:33:26 +0530

    Fibonacci Series

---
+++ fibS.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+void fib(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << fibo(i) << " ";
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    fib(n);
+    return 0;
+}
