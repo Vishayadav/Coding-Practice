be7d184c17fb9a9342881de1622da80e06138673
Visha Yadav <visha2.yadav1@gmail.com>
2026-06-21 13:25:49 +0530

    Reverse an Array

---
+++ reverseArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[n - i - 1];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    return 0;
+}
