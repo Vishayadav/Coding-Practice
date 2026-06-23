c0baaa7f43ed6b6b6f1362ac0f4054857378f89b
Visha Yadav <visha2.yadav1@gmail.com>
2026-06-23 12:19:11 +0530

    Remove Duplicates - Sorted Array

---
+++ TUF2.cpp
@@ -0,0 +1,41 @@
+// Assuming that a sorted array is given as an input
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    // Input
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    // Processing
+    unordered_map<int, int> mp;
+    vector<int> ans;
+    for (int i = 0; i < n; i++)
+    {
+        if (mp.find(v[i]) != mp.end())
+        {
+            mp[v[i]]++;
+            continue;
+        }
+        else
+        {
+            mp[v[i]] = 1;
+            ans.push_back(v[i]);
+        }
+    }
+
+    // Output
+    for (int i : ans)
+        cout << i << " ";
+
+    return 0;
+}
