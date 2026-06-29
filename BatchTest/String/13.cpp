83f02c522c24ae948a66aea94323eddbf55b3b3a
Visha Yadav <visha2.yadav1@gmail.com>
2026-06-30 01:52:20 +0530

    S13

---
+++ BatchTest/String/13.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    a == b ? cout << "Same" : cout << "False";
+    return 0;
+}
