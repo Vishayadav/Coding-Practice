3cfe7eee7af87fe73109aaa3192ad5b434f97b61
Visha Yadav <visha2.yadav1@gmail.com>
2026-06-29 19:55:50 +0530

    B1

---
+++ BatchTest/1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        cout << v[i] << " ";
+    }
+    return 0;
+}
