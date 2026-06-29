6b45fd8b05d40ddf519d17f920586c0c2be8a82c
Visha Yadav <visha2.yadav1@gmail.com>
2026-06-29 23:41:23 +0530

    B11

---
+++ BatchTest/11.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    string line, token;
+    getline(cin, line);
+    stringstream ss(line);
+    vector<string> v;
+    while (ss >> token)
+    {
+        v.push_back(token);
+    }
+    sort(v.begin(), v.end());
+    for (auto i : v)
+        cout << i << " ";
+    return 0;
+}
