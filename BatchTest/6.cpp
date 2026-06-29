c4603a731be52d19a1c1b4bce60516bd3d136b3b
Visha Yadav <visha2.yadav1@gmail.com>
2026-06-29 23:17:28 +0530

    B6

---
+++ BatchTest/6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+    ;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        sum += v[i];
+    }
+    cout << sum;
+    return 0;
+}
