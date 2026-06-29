08477ccfdd3954b44d84a553297da35b18eda528
Visha Yadav <visha2.yadav1@gmail.com>
2026-06-30 00:53:45 +0530

    S3

---
+++ BatchTest/String/3.cpp
@@ -0,0 +1,20 @@
+// No. of words in a line
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    string line, token;
+    getline(cin, line);
+    stringstream ss(line);
+    int count = 0;
+    while (ss >> token)
+    {
+        count += token.length();
+    }
+    cout << count;
+    return 0;
+}
