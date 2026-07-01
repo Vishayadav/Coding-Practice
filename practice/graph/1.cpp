a1a18b1d6e11f82e99fe02c240a1465ad6566851
Visha Yadav <visha2.yadav1@gmail.com>
2026-07-01 17:34:58 +0530

    BFS Traversal

--- practice/graph/1.cpp
+++ practice/graph/1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -10,27 +11,55 @@ public:
     int v;
     list<int> *l;
 
+    graph(int v)
+    {
+        this->v = v;
+        this->l = new list<int>[v];
+    }
     void addEdge(int u, int v)
     {
         l[u].push_back(v);
         l[v].push_back(u);
     }
-    graph(int v)
-    {
-        this->v = v;
-        l = new list<int>[v];
-    }
-
-    void printAdjList()
+    void printList()
     {
         for (int i = 0; i < v; i++)
         {
             cout << i << " : ";
-            for (int j : l[i])
+            for (int neigh : l[i])
             {
-                cout << j << " ";
+                cout << neigh << " ";
             }
             cout << endl;
+        }
+    }
+    void traversal()
+    {
+        if (v <= 0)
+            return;
+        queue<int> q;
+        vector<int> visited(v, 0);
+        for (int i = 0; i < v; i++)
+        {
+            if (!visited[i])
+            {
+                visited[i] = 1;
+                q.push(i);
+                while (!q.empty())
+                {
+                    int curr = q.front();
+                    q.pop();
+                    cout << curr << " ";
+                    for (int neigh : l[curr])
+                    {
+                        if (!visited[neigh])
+                        {
+                            visited[neigh] = 1;
+                            q.push(neigh);
+                        }
+                    }
+                }
+            }
         }
     }
 };
@@ -41,9 +70,9 @@ int main()
     g1.addEdge(0, 1);
     g1.addEdge(1, 2);
     g1.addEdge(2, 3);
-    g1.addEdge(4, 1);
     g1.addEdge(3, 4);
-
-    g1.printAdjList();
+    g1.addEdge(4, 1);
+    g1.printList();
+    g1.traversal();
     return 0;
 }
