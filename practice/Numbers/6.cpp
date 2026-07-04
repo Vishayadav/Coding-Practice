76771a7a4862f2b29d2c5558e6c4f824869c454c
Visha Yadav <visha2.yadav1@gmail.com>
2026-07-04 10:22:01 +0530

    Perfect Number

---
+++ practice/Numbers/6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+bool perfect(int n)
+{
+    int sum = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+            sum += i;
+    }
+    return sum == n;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << perfect(n);
+    return 0;
+}
