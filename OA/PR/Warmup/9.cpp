2e5be3cb1e74d0f7f1772d3010ed043a82d5f166
Visha Yadav <visha2.yadav1@gmail.com>
2026-07-11 19:04:00 +0530

    Sum of digits - Negatives allowed

---
+++ OA/PR/Warmup/9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    n = abs(n);
+    int sum = 0;
+    while (n > 0)
+    {
+        int digit = n % 10;
+        sum += digit;
+        n /= 10;
+    }
+    cout << sum;
+}
