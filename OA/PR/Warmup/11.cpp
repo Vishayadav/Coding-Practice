248d0eeda69e6526978e9961753821020377f429
Visha Yadav <visha2.yadav1@gmail.com>
2026-07-11 19:48:29 +0530

    GCD of 2 numbers:

---
+++ OA/PR/Warmup/11.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    a = abs(a), b = abs(b);
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b);
+    return 0;
+}
