8e37725810e3ace3615228aadde4c754b9371975
Visha Yadav <visha2.yadav1@gmail.com>
2026-07-11 17:52:21 +0530

    Nth term of fibonacci series

---
+++ OA/PR/Warmup/4.cpp
@@ -0,0 +1,20 @@
+// nth term of fibonacci series
+//  0 1 2 3 5 8 13 ....
+#include <iostream>
+
+using namespace std;
+
+int fib(int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fib(n);
+    return 0;
+}
