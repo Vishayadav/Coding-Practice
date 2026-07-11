c920730cc0c48b519d7991af2151357ea3386a18
Visha Yadav <visha2.yadav1@gmail.com>
2026-07-12 01:08:54 +0530

    Array subset - Frquency considered

---
+++ OA/PR/Warmup/24.cpp
@@ -0,0 +1,31 @@
+// Checking of b is a subset of a or not. (Frequency considered).
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    auto left = a, right = b;
+    for (int i = 0; i < m; i++)
+    {
+        if (left[i] != right[i])
+        {
+            cout << "No";
+            return 0;
+        }
+    }
+    cout << "Yes";
+    return 0;
+}
