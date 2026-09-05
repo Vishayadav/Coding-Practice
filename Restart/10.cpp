982675456d5faa1c1d33cc348443211c113b0aa3
Visha Yadav <visha2.yadav1@gmail.com>
2026-09-05 12:54:23 +0530

    Buy and sell stocks - Multiple transactiosn allowed (Updated)

--- Restart/10.cpp
+++ Restart/10.cpp
@@ -11,35 +11,25 @@ public:
     int maxProfit(vector<int> &p)
     {
         // code here
-        int maxProfit = INT_MIN, currMini = INT_MAX, profit, ans = 0;
+        int maxProfit = 0, currMini = INT_MAX, profit, ans = 0;
         for (int i = 0; i < p.size(); i++)
         {
             if (p[i] < currMini || p[i] < p[i - 1])
             {
                 // New buy
-                if (maxProfit == INT_MIN)
-                {
-                    cout << "Entered maxProfit = INT_MIN, made ans =0 (Intitial Buy)" << endl;
-                    ans = 0;
-                }
-                else
-                {
-                    cout << "Enetered new buy , p[i] < currMini and maxProfit != INT_MIN" << endl;
-                    cout << "Ans: " << ans << "MaxProfit: " << endl;
-                    ans += maxProfit;
-                    maxProfit = 0;
-                }
+                ans += maxProfit;
+                maxProfit = 0;
                 currMini = p[i];
             }
             // Calculate profit:
             profit = p[i] - currMini;
             maxProfit = max(maxProfit, profit);
         }
-        cout << "ans: " << ans << "maxprofit: " << maxProfit << endl;
         ans += maxProfit;
         return ans;
     }
 };
+
 int main()
 {
     int n;
